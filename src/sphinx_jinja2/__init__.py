cf87e82b6ceaffcc0b271cf578be1a420baade75
Václav Šmilauer <eu@doxos.eu>
2024-09-26 23:14:36 +0200

    (fixes)

--- src/sphinx_jinja2/__init__.py
+++ src/sphinx_jinja2/__init__.py
@@ -88,6 +88,8 @@ class JinjaOptions(TypedDict, total=False):
     relative to current file, or src directory (if starts with ``/``) """
     debug: bool
     """Also output the rendered template"""
+    raw: str
+    """The rendered template is raw output in given format, rather than input put back into the source parser."""
 
 
 class JinjaDirective(SphinxDirective):
@@ -197,9 +199,7 @@ class JinjaDirective(SphinxDirective):
             )
             self.state_machine.insert_input(new_lines, source)
         else:
-            raw_node = nodes.raw(
-                "", new_content, classes="jinja-rendered", format=self.options["raw"]
-            )
+            raw_node = nodes.raw('', new_content, classes=['jinja-rendered'], format=self.options['raw'])
             (raw_node.source, raw_node.line) = self.state_machine.get_source_and_line(self.lineno)
             return [raw_node]
 
